0f76c79f2c2edd52e4e018d2f87f00e968ca2b85
Goncharenko Andrey <goncharenkoandrey23@ukr.net>
2021-01-10 14:01:03 +0200

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+const server = require("./lib/server");
+const workers = require("./lib/workers");
+const cli = require("./lib/cli");
+const app = {
+	init: function(){
+		server.init();
+		//workers.init();
+		setTimeout(function(){
+			cli.init();
+		}, 50);
+	}
+};
+app.init();
+module.exports = app;
